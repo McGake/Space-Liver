8786254efd6b8157a780fc7137d7f5e872f7a90c
Blake <ldblakeyates@gmail.com>
2018-05-07 20:50:50 -0500

    space man rotates. working on making arms controllable yet flopy.

---
+++ Unity_Project/TryToLive/Assets/Scripts/SpaceMan/SpaceManController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpaceManController : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+    Vector2 startDrift = new Vector2(5,4);
+    public GameObject rightArm;
+    public GameObject leftArm;
+
+	// Use this for initialization
+	void Awake () {
+		rb2D = gameObject.GetComponent<Rigidbody2D>();
+        rb2D.AddForce(startDrift);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RotateInSpace();
+
+
+
+    }
+
+    private float horizontal;
+
+    private void RotateInSpace()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        rb2D.angularVelocity = horizontal * -100;
+
+
+    }
+}
