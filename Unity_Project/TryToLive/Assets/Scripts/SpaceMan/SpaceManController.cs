01a2c72584cca377e6b97df06ea42d8ccc21793b
Blake <ldblakeyates@gmail.com>
2018-05-09 21:14:59 -0500

    arm changes

--- Unity_Project/TryToLive/Assets/Scripts/SpaceMan/SpaceManController.cs
+++ Unity_Project/TryToLive/Assets/Scripts/SpaceMan/SpaceManController.cs
@@ -10,7 +10,7 @@ public class SpaceManController : MonoBehaviour {
     public GameObject rightArmJoint;
     public GameObject leftArmJoint;
 
-    //public GameObject bloopTester;
+    public GameObject bloopTester;
 
 	// Use this for initialization
 	void Awake () {
@@ -53,21 +53,26 @@ public class SpaceManController : MonoBehaviour {
     {
         horizontal2 = Input.GetAxis("Horizontal2");
         vertical2 = Input.GetAxis("Vertical2");
-        //tempPosBloop.y = transform.position.y + -vertical2;
-        //tempPosBloop.x = transform.position.x + horizontal2;
-        //bloopTester.transform.position = tempPosBloop;
+        tempPosBloop.y = transform.position.y + -vertical2;
+        tempPosBloop.x = transform.position.x + horizontal2;
+        bloopTester.transform.position = tempPosBloop;
 
         direction.y = horizontal2;
         direction.x = -vertical2;
-        direction.Normalize();
+        if ((Mathf.Abs(horizontal2) + Mathf.Abs(vertical2)) > .1f)
+        {
 
-        angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
 
-        targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            direction.Normalize();
 
-        leftArmJoint.transform.rotation = Quaternion.Slerp(leftArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+            angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
 
-        rightArmJoint.transform.rotation = Quaternion.Slerp(rightArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+            targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            leftArmJoint.transform.rotation = Quaternion.Slerp(leftArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+
+            rightArmJoint.transform.rotation = Quaternion.Slerp(rightArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+        }
 
 
     }
