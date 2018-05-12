0c52afaf238928f28754e3a4c2083465974fd702
Blake <ldblakeyates@gmail.com>
2018-05-11 23:34:13 -0500

    arm rotation with mouse click now also

--- Unity_Project/TryToLive/Assets/Scripts/SpaceMan/SpaceManController.cs
+++ Unity_Project/TryToLive/Assets/Scripts/SpaceMan/SpaceManController.cs
@@ -12,6 +12,8 @@ public class SpaceManController : MonoBehaviour {
 
     public GameObject bloopTester;
 
+    public Camera cam;
+
 	// Use this for initialization
 	void Awake () {
 		rb2D = gameObject.GetComponent<Rigidbody2D>();
@@ -53,17 +55,45 @@ public class SpaceManController : MonoBehaviour {
     {
         horizontal2 = Input.GetAxis("Horizontal2");
         vertical2 = Input.GetAxis("Vertical2");
-        tempPosBloop.y = transform.position.y + -vertical2;
-        tempPosBloop.x = transform.position.x + horizontal2;
-        bloopTester.transform.position = tempPosBloop;
 
-        direction.y = horizontal2;
-        direction.x = -vertical2;
         if ((Mathf.Abs(horizontal2) + Mathf.Abs(vertical2)) > .1f)
         {
-            direction.Normalize();
+
+           tempPosBloop.y = transform.position.y + -vertical2;
+            tempPosBloop.x = transform.position.x + horizontal2;
+           bloopTester.transform.position = tempPosBloop;
+
+            direction.y = horizontal2;
+            direction.x = -vertical2;
+
 
             angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+            targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            leftArmJoint.transform.rotation = Quaternion.Slerp(leftArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+
+            rightArmJoint.transform.rotation = Quaternion.Slerp(rightArmJoint.transform.rotation, targetRotation, armRotSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetButton("LMB"))
+        {
+            Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+
+            direction.y = mousePos.y - transform.position.y;
+            direction.x = mousePos.x - transform.position.x;
+
+
+            Debug.Log((mousePos.x - transform.position.x) + " / " + (mousePos.y - transform.position.y));
+           
+
+            tempPosBloop.x = direction.x;
+            tempPosBloop.y = direction.y;
+            bloopTester.transform.position = tempPosBloop;
+
+            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            //Debug.Log(angle);
 
             targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
